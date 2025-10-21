5766fc01987c738371bfccbb8cb42503b6ad045f
mikilamb90 <mikilamb90@users.noreply.github.com>
2025-10-21 16:02:00 +0200

    create a dev env

--- main.tf
+++ main.tf
@@ -17,42 +17,45 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
+
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning terraform"
   }
+}
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  name = "blog_new"
+  vpc_id = module.blog_vpc.vpc_id
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and htpps in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
-}
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
-  security_group_id = aws_security_group.blog.id
-}
-resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
-  security_group_id = aws_security_group.blog.id
-}
-resource "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0]
-  security_group_id = aws_security_group.blog.id
 }
